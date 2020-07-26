63fa5287f67b2b119481a845e98d5444610f9e90
Ranger <15280970040@163.com>
2020-07-26 10:53:04 +0800

    feat: code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  extends: [require.resolve('com.vscot.fabric/lib/tslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
     REACT_APP_ENV: true,
+  },
+  parserOptions: {
+    project: path.join(__dirname, './tsconfig.json'),
   },
 };
